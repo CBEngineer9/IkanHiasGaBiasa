0f68297a694d24cc20f7dad9fe5aa849a24453c4
CBEngineer <CBEalexanderkevin@gmail.com>
2021-11-10 01:06:21 +0700

    conn to dbserver

--- proyekpw_lib.php
+++ proyekpw_lib.php
@@ -1,9 +1,15 @@
 <?php
         session_start();
-        $servername = "localhost";
-        $username = "proyekpw";
-        $password = "proyekpw";
-        $dbname = "proyekpw";
+        // $servername = "localhost";
+        // $username = "proyekpw";
+        // $password = "proyekpw";
+        // $dbname = "proyekpw";
+
+        // SERVER
+        $servername = "103.55.39.181:3306";
+        $username = "ikanhia1_proyekpw";
+        $password = "ikanhia1_proyekpw";
+        $dbname = "ikanhia1_proyekpw";
 
         function alert(String $message = null)
         {
