033753598397f662628ed58d501fe82a4ebbd363
Cung1-2 <andrewloekito12345@gmail.com>
2021-11-23 23:14:00 +0700

    website cleanup

--- index.php
+++ index.php
@@ -150,75 +150,56 @@
         <div class="row">
             <div class="col-md-9">
                 <div class="well well-lg offer-box text-center">
-
-
-                   Today's Offer : &nbsp; <span class="glyphicon glyphicon-cog"></span>&nbsp;40 % off  on purchase of $ 2,000 and above till 24 dec !                
-              
-               
+                   Welcome, Dear Customer!   
                 </div>
                 <div class="main box-border">
                     <div id="mi-slider" class="mi-slider">
                         <ul>
                             <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan1.png" alt="img01"><h4>Boots</h4>
+                                <img src="assets/ItemSlider/images/ikan1.png" alt="img01"><h4>Penguin</h4>
                             </a></li>
                             <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan2.png" alt="img02"><h4>Oxfords</h4>
+                                <img src="assets/ItemSlider/images/ikan2.png" alt="img02"><h4>Discus</h4>
                             </a></li>
                             <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan3.png" alt="img03"><h4>Loafers</h4>
+                                <img src="assets/ItemSlider/images/ikan3.png" alt="img03"><h4>Guppy</h4>
                             </a></li>
                             <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan4.png" alt="img04"><h4>Sneakers</h4>
-                            </a></li>
-                        </ul>
-                        <ul>
-                            <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan5.png" alt="img05"><h4>Belts</h4>
-                            </a></li>
-                            <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan6.png" alt="img06"><h4>Hats &amp; Caps</h4>
-                            </a></li>
-                            <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan7.png" alt="img07"><h4>Sunglasses</h4>
-                            </a></li>
-                            <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan8.png" alt="img08"><h4>Scarves</h4>
+                                <img src="assets/ItemSlider/images/ikan4.png" alt="img04"><h4>Cupang</h4>
                             </a></li>
                         </ul>
                         <ul>
                             <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan9.png" alt="img09"><h4>Casual</h4>
+                                <img src="assets/ItemSlider/images/ikan5.png" alt="img05"><h4>Komet</h4>
                             </a></li>
                             <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan10.png" alt="img10"><h4>Luxury</h4>
+                                <img src="assets/ItemSlider/images/ikan6.png" alt="img06"><h4>Louhan</h4>
                             </a></li>
                             <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan11.png" alt="img11"><h4>Sport</h4>
+                                <img src="assets/ItemSlider/images/ikan7.png" alt="img07"><h4>Koki</h4>
                             </a></li>
                             <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan16.png" alt="img11"><h4>Sport</h4>
+                                <img src="assets/ItemSlider/images/ikan8.png" alt="img08"><h4>Koi</h4>
                             </a></li>
                         </ul>
                         <ul>
-                            <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan12.png" alt="img12"><h4>Carry-Ons</h4>
+                        <li><a href="#">
+                                <img src="assets/ItemSlider/images/ikan12.png" alt="img12"><h4>Arwana</h4>
                             </a></li>
                             <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan13.png" alt="img13"><h4>Duffel Bags</h4>
+                                <img src="assets/ItemSlider/images/ikan13.png" alt="img13"><h4>Neon Tetra</h4>
                             </a></li>
                             <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan14.png" alt="img14"><h4>Laptop Bags</h4>
+                                <img src="assets/ItemSlider/images/ikan14.png" alt="img14"><h4>Mengbadut</h4>
                             </a></li>
                             <li><a href="#">
-                                <img src="assets/ItemSlider/images/ikan15.png" alt="img15"><h4>Briefcases</h4>
+                                <img src="assets/ItemSlider/images/ikan15.png" alt="img15"><h4>Tetra</h4>
                             </a></li>
                         </ul>
                         <nav>
-                            <a href="#">Shoes</a>
-                            <a href="#">Accessories</a>
-                            <a href="#">Watches</a>
-                            <a href="#">Bags</a>
+                            <a href="#">Ikan Biru</a>
+                            <a href="#">Ikan Bawa Hoki</a>
+                            <a href="#">Ikan Agak RGB</a>
                         </nav>
                     </div>
                     
@@ -228,27 +209,22 @@
             <!-- /.col -->
             
             <div class="col-md-3 text-center">
-                <div class=" col-md-12 col-sm-6 col-xs-6" >
-                    <div class="offer-text">
-                        30% off here
-                    </div>
+                <div class=" col-md-12 col-sm-12 col-xs-12" >
                     <div class="thumbnail product-box">
                         <img src="assets/img/ikan/ikan5.png" alt="" />
                         <div class="caption">
-                            <h3><a href="#">Samsung Galaxy </a></h3>
-                            <p><a href="#">Ptional dismiss button </a></p>
+                            <h3><a href="#">Ikan Komet</a></h3>
+                            <p><a href="#">Ikan Komet</a></p>
                         </div>
                     </div>
                 </div>
-                <div class=" col-md-12 col-sm-6 col-xs-6">
-                    <div class="offer-text2">
-                        30% off here
-                    </div>
+                <div class=" col-md-12 col-sm-12 col-xs-12">
+                    
                     <div class="thumbnail product-box">
                         <img src="assets/img/ikan/ikan7.png" alt="" />
                         <div class="caption">
-                            <h3><a href="#">Samsung Galaxy </a></h3>
-                            <p><a href="#">Ptional dismiss button </a></p>
+                            <h3><a href="#">Ikan Koki </a></h3>
+                            <p><a href="#">Ikan Mas Koki</a></p>
                         </div>
                     </div>
                 </div>
@@ -270,79 +246,18 @@
                                 <!-- TODO coloring -->
                             </li>
                         <?php }?>
-                        
-                        <!-- <li class="list-group-item">Mobile
-                            <span class="label label-primary pull-right">234</span>
-                        </li>
-                        <li class="list-group-item">Computers
-                            <span class="label label-success pull-right">34</span>
-                        </li>
-                        <li class="list-group-item">Tablets
-                            <span class="label label-danger pull-right">4</span>
-                        </li>
-                        <li class="list-group-item">Appliances
-                            <span class="label label-info pull-right">434</span>
-                        </li>
-                        <li class="list-group-item">Games & Entertainment
-                            <span class="label label-success pull-right">34</span>
-                        </li> -->
-
                     </ul>
                 </div>
-                <!-- /.div -->
                 <!-- <div>
-                    <a href="#" class="list-group-item active list-group-item-success">Clothing & Wears
-                    </a>
-                    <ul class="list-group">
-
-                        <li class="list-group-item">Men's Clothing
-                             <span class="label label-danger pull-right">300</span>
-                        </li>
-                        <li class="list-group-item">Women's Clothing
-                             <span class="label label-success pull-right">340</span>
-                        </li>
-                        <li class="list-group-item">Kid's Wear
-                             <span class="label label-info pull-right">735</span>
-                        </li>
-
-                    </ul>
-                </div> -->
-                <!-- /.div -->
-                <!-- <div>
-                    <a href="#" class="list-group-item active">Accessaries & Extras
-                    </a>
-                    <ul class="list-group">
-                        <li class="list-group-item">Mobile Accessaries
-                             <span class="label label-warning pull-right">456</span>
-                        </li>
-                        <li class="list-group-item">Men's Accessaries
-                             <span class="label label-success pull-right">156</span>
-                        </li>
-                        <li class="list-group-item">Women's Accessaries
-                             <span class="label label-info pull-right">400</span>
-                        </li>
-                        <li class="list-group-item">Kid's Accessaries
-                             <span class="label label-primary pull-right">89</span>
-                        </li>
-                        <li class="list-group-item">Home Products
-                             <span class="label label-danger pull-right">90</span>
-                        </li>
-                        <li class="list-group-item">Kitchen Products
-                             <span class="label label-warning pull-right">567</span>
-                        </li>
-                    </ul>
-                </div> -->
-                <!-- /.div -->
-                <div>
                     <ul class="list-group">
                         <li class="list-group-item list-group-item-success"><a href="#">New Offer's Coming </a></li>
                         <li class="list-group-item list-group-item-info"><a href="#">New Products Added</a></li>
                         <li class="list-group-item list-group-item-warning"><a href="#">Ending Soon Offers</a></li>
                         <li class="list-group-item list-group-item-danger"><a href="#">Just Ended Offers</a></li>
                     </ul>
-                </div>
+                </div> -->
                 <!-- /.div -->
-                <div class="well well-lg offer-box offer-colors">
+                <!-- <div class="well well-lg offer-box offer-colors">
 
 
                     <span class="glyphicon glyphicon-star-empty"></span>25 % off  , GRAB IT                 
@@ -357,7 +272,7 @@
                         </div>
                     </div>
                     <a href="#">click here to know more </a>
-                </div>
+                </div> -->
                 <!-- /.div -->
             </div>
             <!-- /.col -->
@@ -500,9 +415,7 @@
     </div>
     <!-- /.container -->
     <div class="col-md-12 download-app-box text-center">
-
-        <span class="glyphicon glyphicon-download-alt"></span>Download Our Android App and Get 10% additional Off on all Products . <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="btn btn-danger btn-lg">DOWNLOAD  NOW</a>
-
+        </span>Shop Responsibly .
     </div>
 
     <!--Footer -->
@@ -545,7 +458,7 @@
             
         </div> -->
         <div class="row">
-            <div class="col-md-4">
+            <!-- <div class="col-md-4">
                 <strong>Send a Quick Query </strong>
                 <hr>
                 <form>
@@ -572,23 +485,20 @@
                         </div>
                     </div>
                 </form>
-            </div>
+            </div> -->
 
             <div class="col-md-4">
-                <strong>Our Location</strong>
-                <hr>
+                <strong>About Us</strong>
                 <p>
-                     234, New york Street,<br />
-                                    Just Location, USA<br />
-                    Call: +09-456-567-890<br>
-                    Email: info@yourdomain.com<br>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, et! Sint dolorem itaque inventore provident sed assumenda dolorum impedit atque iure harum quae nisi omnis blanditiis est porro voluptatibus placeat explicabo sit, excepturi, mollitia animi. Itaque veniam expedita ratione dolorum in nihil praesentium magnam voluptates eaque, molestias consectetur accusamus tenetur. Minima molestias asperiores fugiat nam voluptate vero culpa pariatur numquam veniam praesentium, maxime odit ducimus! Omnis repellat qui, repellendus odit maiores impedit ab similique neque, ullam doloribus iure nulla sunt?
                 </p>
 
                 2014 www.yourdomain.com | All Right Reserved
             </div>
             <div class="col-md-4 social-box">
                 <strong>We are Social </strong>
-                <hr>
+                <br>
+                <br>
                 <a href="#"><i class="fa fa-facebook-square fa-3x "></i></a>
                 <a href="#"><i class="fa fa-twitter-square fa-3x "></i></a>
                 <a href="#"><i class="fa fa-google-plus-square fa-3x c"></i></a>
@@ -697,7 +607,7 @@
                                 )
                                 .append(
                                     $("<p>")
-                                    .html('<a href="#">Ptional dismiss button </a>')
+                                    .html( ikan['description'])
                                 )
                                 .append(
                                     $("<p>")
